491054e61ef231810a1c904f79d7fb11ebe10f54
Ahsan Arshad <88687816+Ahsanarsha@users.noreply.github.com>
2023-11-01 19:41:11 +0500

    first commit

---
+++ lib/screens/register/bloc/register_bloc.dart
@@ -0,0 +1,43 @@
+import 'package:card_club/resources/repoitory.dart';
+
+class RegisterBloc {
+  final _repository = Repository();
+  var _registerReq;
+  var _otpReq;
+
+  dynamic get registerModel => _registerReq;
+  dynamic get otpModel => _otpReq;
+
+  otpRequest(var email) async {
+    dynamic otpModel = await _repository.sendOtpRequest(email);
+    _otpReq = otpModel;
+  }
+
+  registerRequest(
+    var name,
+    var email,
+    var password,
+    var passwordConfirmation,
+    var deviceKey,
+    var token,
+    var phoneNum,
+  ) async {
+    dynamic registerModel = await _repository.sendRegisterRequest(
+      name,
+      email,
+      password,
+      passwordConfirmation,
+      deviceKey,
+      token,
+      phoneNum,
+    );
+    _registerReq = registerModel;
+  }
+
+  dispose() {
+    _registerReq.close();
+    _otpReq.close();
+  }
+}
+
+final registerBloc = RegisterBloc();
